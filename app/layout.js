1b2ab0bf23ac44da3db7a97813aa699a316e5fbb
VikramanR <vkram777@gmail.com>
2025-06-03 16:05:04 +0530

    created models and finished homepage

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,7 @@
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -10,17 +14,32 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <main>{children}</main>
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>
-              &copy; {new Date().getFullYear()} Medimeet. All rights reserved.
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>
+                  &copy; {new Date().getFullYear()} Medimeet. All rights
+                  reserved.
+                </p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
